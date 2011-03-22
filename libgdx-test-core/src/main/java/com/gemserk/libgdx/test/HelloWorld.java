e72ae25c00d22dd51054cf43eb684667f65f22f7
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-22 12:10:38 -0300

    added a color converter and added an image of gemserk logo

--- libgdx-test-core/src/main/java/com/gemserk/libgdx/test/HelloWorld.java
+++ libgdx-test-core/src/main/java/com/gemserk/libgdx/test/HelloWorld.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.gemserk.animation4j.converters.TypeConverter;
-import com.gemserk.animation4j.time.UpdateableTimeProvider;
+import com.gemserk.animation4j.interpolator.function.InterpolatorFunctionFactory;
 import com.gemserk.animation4j.transitions.Transition;
 import com.gemserk.animation4j.transitions.Transitions;
 
@@ -24,21 +24,22 @@ public class HelloWorld implements ApplicationListener {
 	BitmapFont font;
 
 	Transition<Vector2> position;
+	
+	Transition<Color> color;
 
 	Vector2 startPosition;
 
 	Vector2 endPosition;
 
-	private UpdateableTimeProvider timeProvider;
-
 	@Override
 	public void create() {
 		font = new BitmapFont();
 		font.setColor(Color.RED);
-		texture = new Texture(Gdx.files.internal("data/badlogic.jpg"));
+		texture = new Texture(Gdx.files.internal("data/logo-gemserk-512x128-white.png"));
 		spriteBatch = new SpriteBatch();
 
 		TypeConverter<Vector2> vector2Converter = new Vector2Converter();
+		ColorConverter colorConverter = new ColorConverter();
 
 		startPosition = new Vector2(0, 100);
 		endPosition = new Vector2(0, 100);
@@ -46,16 +47,11 @@ public class HelloWorld implements ApplicationListener {
 		// position = new AutoUpdateableTransition<Vector2>(startPosition, endPosition, //
 		// new GenericInterpolator<Vector2>(vector2Converter, new FloatArrayInterpolator(2)), 0.001f);
 
-		timeProvider = new UpdateableTimeProvider();
-
-		position = Transitions.transition(startPosition, vector2Converter);
+		position = Transitions.transition(startPosition, vector2Converter, InterpolatorFunctionFactory.easeOut(), InterpolatorFunctionFactory.easeIn());
+		color = Transitions.transition(new Color(1f,1f,1f,1f), colorConverter);
 		
-//		position = new AutoUpdateableTransition<Vector2>(new Vector2(startPosition), //
-//				Interpolators.interpolator(vector2Converter), 0.001f);
-		// position.set(new Vector2(endPosition), 1000);
-
 	}
-	
+
 	Random random = new Random();
 
 	@Override
@@ -66,20 +62,22 @@ public class HelloWorld implements ApplicationListener {
 		Gdx.graphics.getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
 
 		Vector2 textPosition = position.get();
-//		Gdx.app.log("HelloWorld", "position " + textPosition + ", desiredPosition: " + endPosition);
+		// Gdx.app.log("HelloWorld", "position " + textPosition + ", desiredPosition: " + endPosition);
 
 		spriteBatch.begin();
-		spriteBatch.setColor(Color.WHITE);
+		spriteBatch.setColor(color.get());
 		spriteBatch.draw(texture, centerX - texture.getWidth() / 2, centerY - texture.getHeight() / 2, 0, 0, texture.getWidth(), texture.getHeight());
+		font.setColor(color.get());
 		font.draw(spriteBatch, "Hello World!", (int) textPosition.x, (int) textPosition.y);
 		spriteBatch.end();
 
-		float deltaTime = Gdx.graphics.getDeltaTime();
-		timeProvider.update((long) (deltaTime * 1000));
-		
+		// float deltaTime = Gdx.graphics.getDeltaTime();
+		// timeProvider.update((long) (deltaTime * 1000));
+
 		if (textPosition.dst2(endPosition) < 1f) {
 			endPosition.set(random.nextFloat() * Gdx.graphics.getWidth(), random.nextFloat() * Gdx.graphics.getHeight());
 			position.set(endPosition, 1000);
+			color.set(new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), random.nextFloat() * 0.2f + 0.7f));
 		}
 
 	}
