777863814b249c1c89a3a480e0fd03a5f259a8b3
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-11 12:32:51 -0300

    modified to use Physics components and systems

--- facehunt-core/src/main/java/com/gemserk/games/facehunt/screens/GameScreen.java
+++ facehunt-core/src/main/java/com/gemserk/games/facehunt/screens/GameScreen.java
@@ -14,28 +14,32 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.gemserk.animation4j.transitions.Transitions;
 import com.gemserk.animation4j.transitions.sync.Synchronizers;
 import com.gemserk.commons.artemis.WorldWrapper;
-import com.gemserk.commons.artemis.components.MovementComponent;
-import com.gemserk.commons.artemis.components.Spatial;
+import com.gemserk.commons.artemis.components.PhysicsComponent;
 import com.gemserk.commons.artemis.components.SpatialComponent;
 import com.gemserk.commons.artemis.components.SpatialImpl;
+import com.gemserk.commons.artemis.components.SpatialPhysicsImpl;
 import com.gemserk.commons.artemis.components.SpriteComponent;
 import com.gemserk.commons.artemis.components.TimerComponent;
 import com.gemserk.commons.artemis.systems.MovementSystem;
+import com.gemserk.commons.artemis.systems.PhysicsSystem;
 import com.gemserk.commons.artemis.systems.RenderLayer;
 import com.gemserk.commons.artemis.systems.SpriteRendererSystem;
 import com.gemserk.commons.artemis.systems.SpriteUpdateSystem;
 import com.gemserk.commons.artemis.systems.TimerSystem;
 import com.gemserk.commons.artemis.triggers.AbstractTrigger;
 import com.gemserk.commons.gdx.ScreenAdapter;
+import com.gemserk.commons.gdx.box2d.BodyBuilder;
 import com.gemserk.commons.gdx.camera.Camera;
 import com.gemserk.commons.gdx.camera.Libgdx2dCamera;
 import com.gemserk.commons.gdx.camera.Libgdx2dCameraTransformImpl;
 import com.gemserk.commons.gdx.resources.LibgdxResourceBuilder;
 import com.gemserk.games.facehunt.FaceHuntGame;
-import com.gemserk.games.facehunt.components.OutsideAreaComponent;
 import com.gemserk.games.facehunt.systems.OutsideAreaTriggerSystem;
 import com.gemserk.resources.ResourceManager;
 import com.gemserk.resources.ResourceManagerImpl;
@@ -62,6 +66,12 @@ public class GameScreen extends ScreenAdapter {
 
 	private static final Color hideColor = new Color(1f, 1f, 1f, 0f);
 	private static final Color showColor = new Color(1f, 1f, 1f, 1f);
+
+	private com.badlogic.gdx.physics.box2d.World physicsWorld;
+
+	private BodyBuilder bodyBuilder;
+
+	private Box2DDebugRenderer box2dDebugRenderer;
 
 	public GameScreen(FaceHuntGame game) {
 		this.game = game;
@@ -109,27 +119,56 @@ public class GameScreen extends ScreenAdapter {
 		renderLayers.add(new RenderLayer(-1000, -100, backgroundLayerCamera));
 		renderLayers.add(new RenderLayer(-100, 100, worldCamera));
 
+		physicsWorld = new com.badlogic.gdx.physics.box2d.World(new Vector2(0f, 0f), false);
+		bodyBuilder = new BodyBuilder(physicsWorld);
+		box2dDebugRenderer = new Box2DDebugRenderer();
+
 		world = new World();
 
 		worldWrapper = new WorldWrapper(world);
 		worldWrapper.addRenderSystem(new SpriteUpdateSystem());
 		worldWrapper.addRenderSystem(new SpriteRendererSystem(renderLayers));
+		worldWrapper.addUpdateSystem(new PhysicsSystem(physicsWorld));
 		worldWrapper.addUpdateSystem(new TimerSystem());
 		worldWrapper.addUpdateSystem(new MovementSystem());
 		worldWrapper.addUpdateSystem(new OutsideAreaTriggerSystem());
 		worldWrapper.init();
 
+		createBorder(viewportWidth * 0.5f, 0, viewportWidth, 10);
+		createBorder(viewportWidth * 0.5f, viewportHeight, viewportWidth, 10);
+
+		createBorder(0, viewportHeight * 0.5f, 10, viewportHeight);
+		createBorder(viewportWidth, viewportHeight * 0.5f, 10, viewportHeight);
+
 		Sprite backgroundSprite = resourceManager.getResourceValue("BackgroundSprite");
 		createStaticSprite(backgroundSprite, viewportWidth * 0.5f, viewportHeight * 0.5f, 1024, 512, 0f, -101, 0.5f, 0.5f, Color.WHITE);
-		createFaceSpawner(new Rectangle(50, 50, Gdx.graphics.getWidth() - 100, Gdx.graphics.getHeight() - 100));
+		createFaceSpawner(new Rectangle(64, 64, viewportWidth - 128, viewportHeight - 128));
 
 		world.loopStart();
+	}
+
+	BodyBuilder getBodyBuilder() {
+		bodyBuilder.reset();
+		return bodyBuilder;
 	}
 
 	void createStaticSprite(Sprite sprite, float x, float y, float width, float height, float angle, int layer, float centerx, float centery, Color color) {
 		Entity entity = world.createEntity();
 		entity.addComponent(new SpatialComponent(new SpatialImpl(x, y, width, height, angle)));
 		entity.addComponent(new SpriteComponent(sprite, layer, new Vector2(centerx, centery), new Color(color)));
+		entity.refresh();
+	}
+
+	void createBorder(float x, float y, float w, float h) {
+		Entity entity = world.createEntity();
+		Body body = getBodyBuilder() //
+				.type(BodyType.StaticBody) //
+				.boxShape(w * 0.5f, h * 0.5f).mass(1f)//
+				.friction(0f)//
+				.userData(entity)//
+				.position(x, y)//
+				.build();
+		entity.addComponent(new PhysicsComponent(body));
 		entity.refresh();
 	}
 
@@ -157,14 +196,14 @@ public class GameScreen extends ScreenAdapter {
 				Vector2 linearVelocity = new Vector2(0f, 0f);
 				linearVelocity.x = MathUtils.random(30f, 100f);
 				linearVelocity.rotate(MathUtils.random(0f, 360f));
-				
+
 				createFace(x, y, linearVelocity, angularVelocity);
-				
+
 				Sound sound = resourceManager.getResourceValue("CritterSpawnedSound");
 				sound.play();
 
 				Gdx.app.log("FaceHunt", "Face spawned at (" + x + ", " + y + ")");
-				
+
 				return false;
 			}
 		}));
@@ -180,33 +219,49 @@ public class GameScreen extends ScreenAdapter {
 
 		Synchronizers.transition(faceColor, Transitions.transitionBuilder(hideColor).end(showColor).time(500).build());
 
-		entity.addComponent(new SpatialComponent(new SpatialImpl(x, y, 64f, 64f, 0f)));
-		entity.addComponent(new SpriteComponent(sprite, 1, new Vector2(0.5f, 0.5f), faceColor));
-		entity.addComponent(new MovementComponent(linearVelocity, angularVelocity));
-		
-		entity.addComponent(new OutsideAreaComponent(new Rectangle(0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight()), new AbstractTrigger() {
-			@Override
-			protected boolean handle(Entity e) {
-				SpatialComponent spatialComponent = e.getComponent(SpatialComponent.class);
-				OutsideAreaComponent outisdeAreaComponent = e.getComponent(OutsideAreaComponent.class);				
-				MovementComponent movementComponent = e.getComponent(MovementComponent.class);
-				
-				Spatial spatial = spatialComponent.getSpatial();
-				Rectangle area = outisdeAreaComponent.getArea();
-				Vector2 velocity = movementComponent.getVelocity();
-				
-				if (spatial.getX() < area.x || spatial.getX() > area.x + area.width)
-					velocity.x = -velocity.x;
+		Body body = getBodyBuilder() //
+				.type(BodyType.DynamicBody) //
+				.circleShape(32f) //
+				.mass(1f)//
+				.friction(0f)//
+				.restitution(1f)//
+				.userData(entity)//
+				.position(x, y)//
+				.build();
 
-				if (spatial.getY() < area.y || spatial.getY() > area.y + area.height)
-					velocity.y = -velocity.y;
-				
-				Sound sound = resourceManager.getResourceValue("CritterBounceSound");
-				sound.play();
-				
-				return false;
-			}
-		}));
+		body.applyAngularImpulse(angularVelocity);
+		body.applyLinearImpulse(linearVelocity, body.getTransform().getPosition());
+
+		entity.addComponent(new PhysicsComponent(body));
+		entity.addComponent(new SpatialComponent(new SpatialPhysicsImpl(body, 64f, 64f)));
+
+		// entity.addComponent(new SpatialComponent(new SpatialImpl(x, y, 64f, 64f, 0f)));
+		entity.addComponent(new SpriteComponent(sprite, 1, new Vector2(0.5f, 0.5f), faceColor));
+		// entity.addComponent(new MovementComponent(linearVelocity, angularVelocity));
+
+		// entity.addComponent(new OutsideAreaComponent(new Rectangle(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()), new AbstractTrigger() {
+		// @Override
+		// protected boolean handle(Entity e) {
+		// SpatialComponent spatialComponent = e.getComponent(SpatialComponent.class);
+		// OutsideAreaComponent outisdeAreaComponent = e.getComponent(OutsideAreaComponent.class);
+		// MovementComponent movementComponent = e.getComponent(MovementComponent.class);
+		//
+		// Spatial spatial = spatialComponent.getSpatial();
+		// Rectangle area = outisdeAreaComponent.getArea();
+		// Vector2 velocity = movementComponent.getVelocity();
+		//
+		// if (spatial.getX() < area.x || spatial.getX() > area.x + area.width)
+		// velocity.x = -velocity.x;
+		//
+		// if (spatial.getY() < area.y || spatial.getY() > area.y + area.height)
+		// velocity.y = -velocity.y;
+		//
+		// Sound sound = resourceManager.getResourceValue("CritterBounceSound");
+		// sound.play();
+		//
+		// return false;
+		// }
+		// }));
 
 		entity.refresh();
 	}
@@ -217,6 +272,9 @@ public class GameScreen extends ScreenAdapter {
 		worldCamera.move(cameraData.getX(), cameraData.getY());
 		worldCamera.rotate(cameraData.getAngle());
 		worldWrapper.render();
+
+		if (Gdx.input.isKeyPressed(Keys.D))
+			box2dDebugRenderer.render(physicsWorld);
 	}
 
 	@Override
@@ -246,6 +304,7 @@ public class GameScreen extends ScreenAdapter {
 		resourceManager.unloadAll();
 		spriteBatch.dispose();
 		spriteBatch = null;
+		physicsWorld.dispose();
 	}
 
 }
