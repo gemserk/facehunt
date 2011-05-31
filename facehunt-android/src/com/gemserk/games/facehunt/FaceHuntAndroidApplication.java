d567a5dd5842bd6ca72f08945e3a3db418a80a72
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-31 11:55:55 -0300

    added wake lock (copied configuration from cuboc from libgdx)

--- facehunt-android/src/com/gemserk/games/facehunt/FaceHuntAndroidApplication.java
+++ facehunt-android/src/com/gemserk/games/facehunt/FaceHuntAndroidApplication.java
@@ -3,13 +3,19 @@ package com.gemserk.games.facehunt;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class FaceHuntAndroidApplication extends AndroidApplication  {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initialize(new FaceHuntGame(), false);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useGL20 = false;
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useWakelock = true;
+		initialize(new FaceHuntGame(), config);
 	}
 	
 }
